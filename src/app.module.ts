dfc44ea7ca1e9b0d8b5852afb701f502900ca2b0
PandaLeg <kurtlansfer@gmail.com>
2023-04-04 17:00:04 +0300

    db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as process from 'process';
+import { DatabaseModule } from './db-config/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
